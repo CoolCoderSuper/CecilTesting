3edbd08829b74f01e6e3ad49c8b9f62150631eaa
Joseph Guenther <joseph@codingcool.ca>
2023-04-18 15:53:36 -0400

    Update to avalonia edit

--- CecilTesting/MainWindow.axaml.vb
+++ CecilTesting/MainWindow.axaml.vb
@@ -1,20 +1,29 @@
 Imports System.IO
-Imports Avalonia
 Imports Avalonia.Controls
+Imports Avalonia.Input
 Imports Avalonia.Interactivity
 Imports Avalonia.Markup.Xaml
 Imports Avalonia.Platform.Storage
+Imports AvaloniaEdit
+Imports AvaloniaEdit.Rendering
+Imports AvaloniaEdit.TextMate
 Imports Microsoft.CodeAnalysis
 Imports Mono.Cecil
+Imports TextMateSharp.Grammars
+Imports Pair = System.Collections.Generic.KeyValuePair(Of Integer, Avalonia.Controls.Control)
 
 Partial Public Class MainWindow
     Inherits Window
 
 #Region "Components"
     Private WithEvents tvAssembly As TreeView
-    Private WithEvents txtCode As TextBox
     Private WithEvents btnOpen As MenuItem
     Private WithEvents btnExit As MenuItem
+    Private WithEvents txtCode As TextEditor
+    Private _textMateInstallation As TextMate.Installation
+    Private _generator As New ElementGenerator()
+    Private _registryOptions As RegistryOptions
+    Private _currentTheme As Integer = CInt(ThemeName.DarkPlus)
 
     Public Sub New()
         InitializeComponent()
@@ -23,9 +32,47 @@ Partial Public Class MainWindow
     Private Sub InitializeComponent()
         AvaloniaXamlLoader.Load(Me)
         tvAssembly = FindControl(Of TreeView)("tvAssembly")
-        txtCode = FindControl(Of TextBox)("txtCode")
         btnOpen = FindControl(Of MenuItem)("btnOpen")
         btnExit = FindControl(Of MenuItem)("btnExit")
+
+        txtCode = Me.FindControl(Of TextEditor)("txtCode")
+        txtCode.HorizontalScrollBarVisibility = Primitives.ScrollBarVisibility.Visible
+        txtCode.Background = Avalonia.Media.Brushes.Transparent
+        txtCode.ShowLineNumbers = True
+        txtCode.ContextMenu = New ContextMenu With {
+                .Items = New List(Of MenuItem) From {
+                            New MenuItem With {
+                            .Header = "Copy",
+                            .InputGesture = New KeyGesture(Key.C, KeyModifiers.Control)
+                            },
+                            New MenuItem With {
+                            .Header = "Paste",
+                            .InputGesture = New KeyGesture(Key.V, KeyModifiers.Control)
+                            },
+                            New MenuItem With {
+                            .Header = "Cut",
+                            .InputGesture = New KeyGesture(Key.X, KeyModifiers.Control)
+                            }
+                }
+        }
+        txtCode.TextArea.Background = Me.Background
+        txtCode.Options.ShowBoxForControlCharacters = True
+        txtCode.Options.ColumnRulerPositions = {80, 100}.ToList
+        txtCode.TextArea.RightClickMovesCaret = True
+        txtCode.TextArea.TextView.ElementGenerators.Add(_generator)
+        _registryOptions = New RegistryOptions(CType(_currentTheme, ThemeName))
+        _textMateInstallation = txtCode.InstallTextMate(_registryOptions)
+        Dim vbLanguage As Language = _registryOptions.GetLanguageByExtension(".vb")
+        Dim scopeName As String = _registryOptions.GetScopeByLanguageId(vbLanguage.Id)
+        _textMateInstallation.SetGrammar(_registryOptions.GetScopeByLanguageId(vbLanguage.Id))
+        [AddHandler](PointerWheelChangedEvent, Sub(o, i)
+                                                   If i.KeyModifiers <> KeyModifiers.Control Then Return
+                                                   If i.Delta.Y > 0 Then
+                                                       txtCode.FontSize += 1
+                                                   Else
+                                                       txtCode.FontSize = If(txtCode.FontSize > 1, txtCode.FontSize - 1, 1)
+                                                   End If
+                                               End Sub, RoutingStrategies.Bubble, True)
     End Sub
 
 #End Region
@@ -81,4 +128,38 @@ Partial Public Class MainWindow
     Private Sub btnExit_Click(sender As Object, e As RoutedEventArgs) Handles btnExit.Click
         Close()
     End Sub
+End Class
+
+Public Class ElementGenerator
+    Inherits VisualLineElementGenerator
+    Implements IComparer(Of Pair)
+    Public controls As List(Of Pair) = New List(Of Pair)()
+
+    ''' <summary>
+    ''' Gets the first interested offset using binary search
+    ''' </summary>
+    ''' <returns>The first interested offset.</returns>
+    ''' <param name="startOffset">Start offset.</param>
+    Public Overrides Function GetFirstInterestedOffset(startOffset As Integer) As Integer
+        Dim pos As Integer = controls.BinarySearch(New Pair(startOffset, Nothing), Me)
+        If pos < 0 Then pos = Not pos
+        If pos < controls.Count Then
+            Return controls(pos).Key
+        Else
+            Return -1
+        End If
+    End Function
+
+    Public Overrides Function ConstructElement(offset As Integer) As VisualLineElement
+        Dim pos As Integer = controls.BinarySearch(New Pair(offset, Nothing), Me)
+        If pos >= 0 Then
+            Return New InlineObjectElement(0, controls(pos).Value)
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Private Function Compare(x As Pair, y As Pair) As Integer Implements IComparer(Of Pair).Compare
+        Return x.Key.CompareTo(y.Key)
+    End Function
 End Class
